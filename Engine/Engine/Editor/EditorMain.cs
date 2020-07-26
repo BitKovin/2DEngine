b5f1f7658ad6ed2a6f9cca0462ed08d48f42b00d
BitKovin <bogdan.yuzhda@gmail.com>
2020-07-27 01:29:03 +0300

    added editor window

---
+++ Engine/Engine/Editor/EditorMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Engine.Editor
+{
+    class EditorMain
+    {
+
+        static Vector2i startPoint;
+        static Vector2i endPoint;
+
+        public static void Start()
+        {
+            Renderer.window.MouseButtonPressed += Window_MouseButtonPressed;
+            Renderer.window.MouseButtonReleased += Window_MouseButtonReleased;
+        }
+
+        public static void Update()
+        {
+            
+        }
+
+        private static void Window_MouseButtonReleased(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private static void Window_MouseButtonPressed(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        public static void Test()
+        {
+            Brush brush = new Brush();
+            brush.SetTexture(new Texture("brush.png"));
+            brush.SetSize(new Vector2i(100, 1000));
+            brush.SetPosition(new Vector2i(200, -500));
+            Level.brushes.Add(brush);
+        }
+
+    }
+}
--- Engine/Engine/Program.cs
+++ Engine/Engine/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using Engine.Editor;
 
 namespace Engine
 {
@@ -7,12 +10,28 @@ namespace Engine
         static void Main(string[] args)
         {
 
+            Thread editor = new Thread(new ThreadStart(Editor));
+            editor.Start();
+            Game();
+        }
+
+        static void Editor()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+
+        static void Game()
+        {
             GameMain.Start();
+            EditorMain.Start();
             while (Renderer.window.IsOpen)
             {
                 GameMain.Update();
+                EditorMain.Update();
             }
-            
         }
     }
 }
